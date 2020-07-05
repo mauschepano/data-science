0bad77740e717d72e43cab9019ea5fb2d6722922
Caroline Schneider <caroline.schneider@di-unternehmer.com>
2020-07-05 11:53:00 +0200

    removes .py file

--- ea13/shakespear.py
+++
@@ -1,6 +0,0 @@
-text_file = sc.textFile('./assets/shakespear_cleaned.txt')
-counts = text_file.flatMap(lambda line: line.split(" ")) \
-             .map(lambda word: (word, 1)) \
-             .reduceByKey(lambda a, b: a + b)
-             
-print(counts)
